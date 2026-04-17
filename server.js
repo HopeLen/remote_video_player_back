cdfbdcaa71fe742b925488dcab4bbe05187ab0f3
Hope <roni.trololo2004@gmail.com>
2026-04-17 13:49:00 +0300

    Created a display for names for those in a room

--- server.js
+++ server.js
@@ -33,8 +33,20 @@ app.set("io", io);
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", async () => {
     console.log(`User ${socket.id} disconnected`);
+    const roomId = socket.data.roomId;
+    if (!roomId) return;
+
+    const sockets = await io.in(roomId).fetchSockets();
+
+    const users = sockets.map((s) => ({
+      id: s.id,
+      name: s.data.name,
+    }));
+
+    io.to(roomId).emit("user-action", socket.id);
+    io.to(roomId).emit("room-users", users);
   });
 
   socket.on("loadUrl", (data) => {
@@ -42,15 +54,44 @@ io.on("connection", (socket) => {
     console.log(data);
 
     socket.to(data.roomId).emit("loadUrl", data);
+    io.to(data.roomId).emit("user-action", socket.id);
   });
 
-  socket.on("joinRoom", (roomId) => {
+  socket.on("joinRoom", async ({ roomId, name }) => {
     socket.join(roomId);
-    console.log(`User: ${socket.id} joined room: ${roomId}`);
+
+    // Store user data ON the socket
+    socket.data.name = name;
+    socket.data.roomId = roomId;
+
+    // Get all sockets in the room
+    const sockets = await io.in(roomId).fetchSockets();
+
+    // Build user list
+    const users = sockets.map((s) => ({
+      id: s.id,
+      name: s.data.name,
+    }));
+
+    // Send full list to everyone (or just the new user if you prefer)
+    io.to(roomId).emit("room-users", users);
+    io.to(roomId).emit("user-action", socket.id);
+    console.log(`User: ${socket.id} named: ${name} joined room: ${roomId}`);
   });
 
-  socket.on("leaveRoom", (roomId) => {
+  socket.on("leaveRoom", async (roomId) => {
     socket.leave(roomId);
+    if (!roomId) return;
+
+    const sockets = await io.in(roomId).fetchSockets();
+
+    const users = sockets.map((s) => ({
+      id: s.id,
+      name: s.data.name,
+    }));
+
+    io.to(roomId).emit("user-action", socket.id);
+    io.to(roomId).emit("room-users", users);
     console.log(`User: ${socket.id} left room: ${roomId}`);
   });
 
